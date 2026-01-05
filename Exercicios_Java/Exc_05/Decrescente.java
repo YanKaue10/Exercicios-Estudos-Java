1530d97e9cc3125e848c4979deae46762110bcda
Yan Kauê <yankaue4@gmail.com>
2026-01-05 20:40:17 -0300

    Exercício 5
    Programa para ler 3 ou mais números e printar em ordem decrescente.

---
+++ Exercicios_Java/Exc_05/Decrescente.java
@@ -0,0 +1,10 @@
+public class Decrescente {
+    public static void main(String[] args) {
+        int[] nmr = {10,20,30};
+        
+        for (int i = nmr.length -1; i >= 0; i--){
+            System.out.println(nmr[i]);
+        }
+    }
+
+}
